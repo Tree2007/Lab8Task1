77dc280c317b88b708f3362e0bfa4b44ac1c8729
Forest <fhaughton6@gmail.com>
2024-11-11 08:17:36 -0500

    hh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        String username = InputHelper.getNonZeroLenString(scan, "Please enter your username:");
+        System.out.println("Your username is: " + username);
     }
 }
